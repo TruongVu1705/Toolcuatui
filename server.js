4d89fb08bfec6a5f42817e573d833d9a98d20d1a
TruongVu1705 <nguyenvuhmc@gmail.com>
2026-08-25 10:01:52 +0700

    Fix: use web_creator client + SOCS consent cookie to bypass page reload on Render

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ const server = http.createServer(async (req, res) => {
                 let ytDlpBaseArgs = [
                     '--no-warnings', '--no-download',
                     '--js-runtimes', 'node',
-                    '--extractor-args', 'youtube:player_skip=webpage',
+                    '--extractor-args', 'youtube:player_skip=webpage;player_client=web_creator,default',
                     '--print', '%(title)s|||%(uploader)s|||%(duration)s|||%(thumbnail)s|||%(formats.:.height)j'
                 ];
                 if (fs.existsSync(path.join(__dirname, 'backend', 'cookies.txt'))) {
@@ -359,7 +359,7 @@ const server = http.createServer(async (req, res) => {
         let ytDlpArgs = [
             '--ffmpeg-location', ffmpegDir,
             '--js-runtimes', 'node',
-            '--extractor-args', 'youtube:player_skip=webpage',
+            '--extractor-args', 'youtube:player_skip=webpage;player_client=web_creator,default',
             '-S', 'vcodec:h264,res,acodec:m4a',
             '-f', `bestvideo[ext=mp4][height<=${resolution}]+bestaudio[ext=m4a]/best[ext=mp4][height<=${resolution}]/best`,
             '--merge-output-format', 'mp4',
@@ -376,7 +376,7 @@ const server = http.createServer(async (req, res) => {
             ytDlpArgs = [
                 '--ffmpeg-location', ffmpegDir,
                 '--js-runtimes', 'node',
-                '--extractor-args', 'youtube:player_skip=webpage',
+                '--extractor-args', 'youtube:player_skip=webpage;player_client=web_creator,default',
                 '-f', `hd/sd/bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best`,
                 '--merge-output-format', 'mp4',
                 '--no-warnings',
